3d5acb0a8145da0e9f178deaf27a2e6440709701
aaron yang <aaron_yang@jieyu.ai>
2022-08-17 14:19:40 +0800

    🐞 fix: can't publish multiple version doc #26
    remove peaceiris/actions-gh-pages@v3

--- repo.sh
+++ repo.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Personal token with full access rights is required to run this scripts
-# Once you got persona token, set enviroment variable GH_TOKEN with it
-
 # Uncomment the following to config github secret used by github workflow. 
 gh secret set PERSONAL_TOKEN --body $GH_TOKEN
 gh secret set PYPI_API_TOKEN --body $PYPI_API_TOKEN
@@ -10,13 +7,9 @@ gh secret set TEST_PYPI_API_TOKEN --body $TEST_PYPI_API_TOKEN
 
 # uncomment the following if you need to setup email notification
 gh secret set BUILD_NOTIFY_MAIL_SERVER --body $BUILD_NOTIFY_MAIL_SERVER
+gh secret set BUILD_NOTIFY_MAIL_PORT --body $BUILD_NOTIFY_MAIL_PORT
 gh secret set BUILD_NOTIFY_MAIL_FROM --body $BUILD_NOTIFY_MAIL_FROM
 gh secret set BUILD_NOTIFY_MAIL_PASSWORD --body $BUILD_NOTIFY_MAIL_PASSWORD
 gh secret set BUILD_NOTIFY_MAIL_RCPT --body $BUILD_NOTIFY_MAIL_RCPT
 gh secret set DINGTALK_ACCESS_TOKEN --body $BUILD_DINGTALK_ACCESS_TOKEN
 gh secret set DINGTALK_SECRET --body $BUILD_DINGTALK_SECRET
-
-# uncomment the following to create repo and push code to github
-# git add ./{{cookiecutter.project_slug}}
-# git commit -m "Initial commit by ppw"
-# gh repo create {{cookiecutter.project_slug}} --public -s ./{{cookiecutter.project_slug}} --push
--- {{cookiecutter.project_slug}}/repo.sh
+++ {{cookiecutter.project_slug}}/repo.sh
@@ -5,11 +5,13 @@
 # Once you got persona token, set enviroment variable GH_TOKEN with it
 
 # uncomment the following to create repo and push code to github
+# gh repo create {{cookiecutter.project_slug}} --public
+# git remote add origin git@github.com:{{cookiecutter.github_username}}/{{cookiecutter.project_slug}}.git
 # git add .
 # pre-commit run --all-files
 # git add .
 # git commit -m "Initial commit by ppw"
-# gh repo create {{cookiecutter.project_slug}} --public -s . --push
+# git branch -M main
 
 # Uncomment the following to config github secret used by github workflow. 
 # gh secret set PERSONAL_TOKEN --body $GH_TOKEN
@@ -18,6 +20,9 @@
 
 # uncomment the following if you need to setup email notification
 # gh secret set BUILD_NOTIFY_MAIL_SERVER --body $BUILD_NOTIFY_MAIL_SERVER
+# gh secret set BUILD_NOTIFY_MAIL_PORT --body $BUILD_NOTIFY_MAIL_PORT
 # gh secret set BUILD_NOTIFY_MAIL_FROM --body $BUILD_NOTIFY_MAIL_FROM
 # gh secret set BUILD_NOTIFY_MAIL_PASSWORD --body $BUILD_NOTIFY_MAIL_PASSWORD
 # gh secret set BUILD_NOTIFY_MAIL_RCPT --body $BUILD_NOTIFY_MAIL_RCPT
+
+# git push -u origin main
